9bcac64fa9b2714b17bb4c73a529edeab619b272
Wintergore <mr.liam@live.com>
2019-07-29 21:09:44 +0100

    Initial Project and TestProject created.

---
+++ StringCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+using String.Calculator;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace String.Calculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers) {
+            throw new NotImplimentedException("Please create a test first")
+        }
+    }
+}

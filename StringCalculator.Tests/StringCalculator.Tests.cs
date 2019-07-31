d634b50fad3655703a644e9fad9fd1e32011a6e3
Wintergore <mr.liam@live.com>
2019-07-31 21:47:46 +0100

    Step 4: Support different delimiters

--- StringCalculator.Tests/StringCalculator.Tests.cs
+++ StringCalculator.Tests/StringCalculator.Tests.cs
@@ -6,48 +6,72 @@ namespace String.Calculator.Tests
     [TestFixture]
     public class StringCalculatorTests
     {
-        private StringCalculator _stringCalculator;
+        private StringCalculator stringCalculator;
         [SetUp]
         public void SetUp()
         {
-            _stringCalculator = new StringCalculator();
+            stringCalculator = new StringCalculator();
         }
 
         [Test]
         [TestCase("", ExpectedResult = 0)]
-        public int Returns_0_When_EmptyString_Input(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_0_When_EmptyString_Input(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1", ExpectedResult = 1)]
-        public int Returns_0_When_Null_Input(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_0_When_Null_Input(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1", ExpectedResult = 1, TestName = "Returns_1_When_1_Input")]
-        public int Returns_1_When_1_Input(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_1_When_1_Input(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1,2", ExpectedResult = 3)]
-        public int Returns_Sum_When_Two_Numbers_Input(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_Sum_When_Two_Numbers_Input(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1,", ExpectedResult = 1)]
-        public int Returns_Sum_When_Trailing_Seperator(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_Sum_When_Trailing_Seperator(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1,X")]
         [TestCase("1,%")]
         [TestCase("1, ")]
-        public void Throws_FormatException_When_Non_Number_Input(string numbers) => Assert.That(() => _stringCalculator.Add(numbers), Throws.TypeOf<FormatException>());
+        [TestCase("//^\n4^6;7;9")]
+        public void Throws_FormatException_When_Non_Number_Input(string numbers) => Assert.That(() => stringCalculator.Add(numbers), Throws.TypeOf<FormatException>());
 
         [Test]
         [TestCase("1\n2,3", ExpectedResult = 6)]
-        public int Returns_Sum_When_New_Line_Between_Numbers(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_Sum_When_New_Line_Between_Numbers(string numbers) => stringCalculator.Add(numbers);
 
         [Test]
         [TestCase("1,2,\n", ExpectedResult = 3)]
         [TestCase("\n,1", ExpectedResult = 1)]
         [TestCase("1,\n", ExpectedResult = 1)]
         [TestCase("1\n2", ExpectedResult = 3)]
-        public int Returns_Sum_When_NewLines_Not_Between_Characters(string numbers) => _stringCalculator.Add(numbers);
+        public int Returns_Sum_When_NewLines_Not_Between_Characters(string numbers) => stringCalculator.Add(numbers);
+
+        [Test]
+        [TestCase("//;\n;1", ExpectedResult = 1)]
+        [TestCase("//;\n;1,2\n3", ExpectedResult = 6)]
+        public int Returns_Sum_When_Custom_Delimiter_Specified(string numbers) => stringCalculator.Add(numbers);
+
+        [Test]
+        [TestCase("//;\n", ExpectedResult = 0)]
+        [TestCase("//;\n\n,;", ExpectedResult = 0)]
+        public int Returns_0_When_Only_Delimiters_Passed(string numbers) => stringCalculator.Add(numbers);
+
+        [Test]
+        [TestCase("//\n1,2", ExpectedResult = 3)]
+        public int Returns_Sum_When_Custom_Delimiter_Initialised_But_Not_Supplied(string numbers) => stringCalculator.Add(numbers);
+
+        [Test]
+        [TestCase("//;2")]
+        public void Throws_FormatException_When_Custom_Delimiter_Ending_Not_Found(string numbers) => Assert.That(() => stringCalculator.ValidateCustomDelimiter(numbers), Throws.TypeOf<FormatException>());
+
+        [Test]
+        [TestCase("//;;\n")]
+        public void Throws_FormatException_When_CustomDelimiter_Exceeds_1_Character(string numbers) => Assert.That(() => stringCalculator.ValidateCustomDelimiter(numbers), Throws.TypeOf<FormatException>());
+
     }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace String.Calculator
 {
     public class StringCalculator
     {
+        private readonly List<char> delimiters = new List<char> { ',', '\n' };
+        private const string customDelimiterIdentifier = "//";
         public int Add(string numbers)
         {
             if (string.IsNullOrEmpty(numbers)) return 0;
-            return numbers.Split(new char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+
+            if (numbers.StartsWith(customDelimiterIdentifier))
+            {
+                ValidateCustomDelimiter(numbers);
+                numbers = GetNumbersExcludingCustomDelimiter(numbers);
+            }
+
+            return numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
             .Select(number => int.Parse(number))
             .Sum();
+        }
+
+        public void ValidateCustomDelimiter(string numbers)
+        {
+            var indexOfNewLine = numbers.IndexOf('\n'); 
+            if (indexOfNewLine == -1) throw new FormatException("Custom delimiter ending not found '\n'");
+            if (indexOfNewLine - customDelimiterIdentifier.Length > 1) throw new FormatException("Custom delimiter exceeds 1 character");
+        }
+
+        public string GetNumbersExcludingCustomDelimiter(string numbers)
+        {
+            delimiters.Add(numbers[customDelimiterIdentifier.Length]);
+            return numbers.Substring(numbers.IndexOf('\n') + 1);
         }
     }
 }
